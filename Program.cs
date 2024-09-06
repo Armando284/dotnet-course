0f8ac09f82851843509df6d5ec143c6831c65618
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-05 20:08:27 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Hello, World!");
+
+var sale = new Sale(15.0f);
+
+Console.WriteLine(sale.GetInfo());
+
+var saleWithTax = new SaleWithTax(20.0f, 1.15f);
+
+Console.WriteLine(saleWithTax.GetInfo());
+
+saleWithTax.Tax = 1.23f;
+Console.WriteLine(saleWithTax.GetInfo("Francia"));
+
+class Sale { 
+    public float Price {  get; private set; }
+
+    public Sale(float price) {
+        Price = price;
+    }
+
+    public virtual string GetInfo()
+    {
+        return "El precio es: " + Price;
+    }
+}
+
+class SaleWithTax: Sale
+{
+    public float Tax { get; set; }
+
+    public SaleWithTax(float price, float tax): base(price)
+    {
+        Tax = tax;
+    }
+
+    public override string GetInfo()
+    {
+        return "El precio es: " + this.Price * Tax;
+    }
+
+    public string GetInfo(string tienda)
+    {
+        return "El precio en la tienda: " + tienda + " es: " + this.Price * Tax;
+    }
+}
