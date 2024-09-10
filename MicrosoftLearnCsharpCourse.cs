387e3bae53639d4c088d7f1ef4934518bfedac59
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-10 17:30:03 -0400

    Adds arrays and loops

--- MicrosoftLearnCsharpCourse.cs
+++ MicrosoftLearnCsharpCourse.cs
@@ -135,35 +135,112 @@
 //    total += 2;
 //}
 
-Random dice = new Random();
+//Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+//int roll1 = dice.Next(1, 7);
+//int roll2 = dice.Next(1, 7);
+//int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
+//int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+//Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+//if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+//{
+//    if ((roll1 == roll2) && (roll2 == roll3))
+//    {
+//        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//        total += 6;
+//    }
+//    else
+//    {
+//        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//        total += 2;
+//    }
+//}
+
+//if (total >= 15)
+//{
+//    Console.WriteLine("You win!");
+//}
+//else
+//{
+//    Console.WriteLine("Sorry, you lose.");
+//}
+
+
+// arrays o matrices en microsoft learn
+
+//string[] fraudulentOrderIDs = new string[3];
+
+//fraudulentOrderIDs[0] = "A123";
+//fraudulentOrderIDs[1] = "B456";
+//fraudulentOrderIDs[2] = "C789";
+//// fraudulentOrderIDs[3] = "D000"; // !: Error out of boundries 
+
+//Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+//Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+//Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+//fraudulentOrderIDs[0] = "F000";
+
+//Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+//string[] fraudulentOrderIDs2 = ["A123", "B456", "C789"];
+//string[] fraudulentOrderIDs3 = { "A123", "B456", "C789" }; // forma antigua, todavia es valida
+
+//string[] names = { "Rowena", "Robin", "Bao" };
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+////int[] inventory = { 200, 450, 700, 175, 250 };
+////int sum = 0;
+////foreach (int items in inventory)
+////{
+////    sum += items;
+////}
+
+////Console.WriteLine($"We have {sum} items in inventory."); // could use a reducer
+
+//int[] inventory = { 200, 450, 700, 175, 250 };
+//int sum = 0;
+//int bin = 0;
+//foreach (int items in inventory)
+//{
+//    sum += items;
+//    bin++;
+//    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+//}
+//Console.WriteLine($"We have {sum} items in inventory.");
+
+//string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+//foreach (string orderID in orderIDs)
+//{
+//    if (orderID.StartsWith('B'))
+//    {
+//        Console.WriteLine(orderID);
+//    }
+//}
+
+Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
 }
-
-if (total >= 15)
+// Print out each orderID
+foreach (var orderID in orderIDs)
 {
-    Console.WriteLine("You win!");
-}
-else
-{
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine(orderID);
 }
