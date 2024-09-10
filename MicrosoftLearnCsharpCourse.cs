1488e0a850454222bcf691e2aa1aaa89a6cb0c14
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-10 17:08:00 -0400

    Adds more conditional flows

--- MicrosoftLearnCsharpCourse.cs
+++ MicrosoftLearnCsharpCourse.cs
@@ -96,14 +96,14 @@
 // * usarlo para inicializar un algoritmo que genera un número diferente cada vez. 
 // * Aunque no es totalmente aleatorio, es suficiente para la mayoría de las aplicaciones.
 // */
-var dice = new Random();
-int rol = dice.Next(1, 7);
-Console.WriteLine(rol);
+//var dice = new Random();
+//int rol = dice.Next(1, 7);
+//Console.WriteLine(rol);
 
-// Overloading methods
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+//// Overloading methods
+//int roll1 = dice.Next();
+//int roll2 = dice.Next(101);
+//int roll3 = dice.Next(50, 101);
 
 //Console.WriteLine($"First roll: {roll1}");
 //Console.WriteLine($"Second roll: {roll2}");
@@ -116,21 +116,54 @@ int roll3 = dice.Next(50, 101);
 
 //Console.WriteLine(largerValue);
 
-int total = roll1 + roll2 + roll3;
+//int total = roll1 + roll2 + roll3;
 
 //Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
+//string message = "The quick brown fox jumps over the lazy dog.";
+//bool result = message.Contains("dog");
+//Console.WriteLine(result);
 
-if (message.Contains("fox"))
-{
-    Console.WriteLine("What does the fox say?");
-}
+//if (message.Contains("fox"))
+//{
+//    Console.WriteLine("What does the fox say?");
+//}
+
+//if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+//{
+//    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//    total += 2;
+//}
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+else
+{
+    Console.WriteLine("Sorry, you lose.");
 }
