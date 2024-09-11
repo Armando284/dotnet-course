e91352242aa5b92d49c1ec4e16ad8dcd05109251
Armando <72764913+Armando284@users.noreply.github.com>
2024-09-11 10:30:49 -0400

    Adds loops for, while and do while

--- MicrosoftLearnCsharpCourse.cs
+++ MicrosoftLearnCsharpCourse.cs
@@ -294,84 +294,127 @@
 // switch 
 // me pregunto si puedo usar objetos literales para sustituir switch como en JS
 
-int employeeLevel = 200;
-string employeeName = "John Smith";
+//int employeeLevel = 200;
+//string employeeName = "John Smith";
 
-string title = "";
+//string title = "";
 
-switch (employeeLevel)
-{
-    case 100:
-        title = "Junior Associate";
-        break;
-    case 200:
-        title = "Senior Associate";
-        break;
-    case 300:
-        title = "Manager";
-        break;
-    case 400:
-        title = "Senior Manager";
-        break;
-    default:
-        title = "Associate";
-        break;
-}
+//switch (employeeLevel)
+//{
+//    case 100:
+//        title = "Junior Associate";
+//        break;
+//    case 200:
+//        title = "Senior Associate";
+//        break;
+//    case 300:
+//        title = "Manager";
+//        break;
+//    case 400:
+//        title = "Senior Manager";
+//        break;
+//    default:
+//        title = "Associate";
+//        break;
+//}
 
-Console.WriteLine($"{employeeName}, {title}");
+//Console.WriteLine($"{employeeName}, {title}");
 
-// SKU = Stock Keeping Unit
-string sku = "01-MN-L";
+//// SKU = Stock Keeping Unit
+//string sku = "01-MN-L";
 
-string[] product = sku.Split('-');
+//string[] product = sku.Split('-');
 
-string type = "";
-string color = "";
-string size = "";
+//string type = "";
+//string color = "";
+//string size = "";
 
-switch (product[0])
-{
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
-    default:
-        type = "Other";
-        break;
-}
+//switch (product[0])
+//{
+//    case "01":
+//        type = "Sweat shirt";
+//        break;
+//    case "02":
+//        type = "T-Shirt";
+//        break;
+//    case "03":
+//        type = "Sweat pants";
+//        break;
+//    default:
+//        type = "Other";
+//        break;
+//}
 
-switch (product[1])
-{
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;
-}
+//switch (product[1])
+//{
+//    case "BL":
+//        color = "Black";
+//        break;
+//    case "MN":
+//        color = "Maroon";
+//        break;
+//    default:
+//        color = "White";
+//        break;
+//}
 
-switch (product[2])
-{
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
-}
+//switch (product[2])
+//{
+//    case "S":
+//        size = "Small";
+//        break;
+//    case "M":
+//        size = "Medium";
+//        break;
+//    case "L":
+//        size = "Large";
+//        break;
+//    default:
+//        size = "One Size Fits All";
+//        break;
+//}
 
-Console.WriteLine($"Product: {size} {color} {type}");
+//Console.WriteLine($"Product: {size} {color} {type}");
+
+//// for
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//    if (i == 7) break; // salir antes de tiempo
+//}
+
+//// Fizzbuzz 🙄
+
+//for (int i = 1; i < 101; i++)
+//{
+//    if ((i % 3 == 0) && (i % 5 == 0))
+//        Console.WriteLine($"{i} - FizzBuzz");
+//    else if (i % 3 == 0)
+//        Console.WriteLine($"{i} - Fizz");
+//    else if (i % 5 == 0)
+//        Console.WriteLine($"{i} - Buzz");
+//    else
+//        Console.WriteLine($"{i}");
+//}
+
+//int hero = 10;
+//int monster = 10;
+
+//Random dice = new Random();
+
+//do
+//{
+//    int roll = dice.Next(1, 11);
+//    monster -= roll;
+//    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+//    if (monster <= 0) continue;
+
+//    roll = dice.Next(1, 11);
+//    hero -= roll;
+//    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+//} while (hero > 0 && monster > 0);
+
+//Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
